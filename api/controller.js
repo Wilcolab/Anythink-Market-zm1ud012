48576a912349eee509de0a295e38967cc264e0b3
Aimen Malik <Aimo.Malik77@gmail.com>
2025-08-13 20:33:24 +0000

    Add power operation handling in client and server

--- api/controller.js
+++ api/controller.js
@@ -16,8 +16,9 @@ exports.calculate = function(req, res) {
     'subtract': function(a, b) { return a - b },
     'multiply': function(a, b) { return a * b },
     'divide':   function(a, b) { return a / b },
-    'power':   function(a, b) { return Math.pow(a, b) }
+    'power':    function(a, b) { return Math.pow(a, b) }
   };
+  
 
   if (!req.query.operation) {
     throw new Error("Unspecified operation");
